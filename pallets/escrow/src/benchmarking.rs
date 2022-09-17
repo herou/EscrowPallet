47a39c9c239296aa053189f6e997724668c1cfb0
Elio Prifti <elioprifti@gmail.com>
2022-09-17 14:12:46 +0200

    Fix runtime benchmarking tests

--- pallets/escrow/src/benchmarking.rs
+++ pallets/escrow/src/benchmarking.rs
@@ -7,14 +7,14 @@ use crate::Pallet as Escrow;
 use frame_benchmarking::{benchmarks, account, whitelisted_caller};
 use frame_system::{EventRecord, RawOrigin};
 use crate::Event;
+use frame_support::sp_runtime::traits::Bounded;
 
-
-/* fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
-	let events = frame_system::Module::<T>::events();
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	let events = frame_system::Pallet::<T>::events();
 	let system_event: <T as frame_system::Config>::Event = generic_event.into();
 	let EventRecord { event, .. } = &events[events.len() - 1];
 	assert_eq!(event, &system_event);
-}*/
+}
 
 
 benchmarks! {
@@ -30,6 +30,6 @@ benchmarks! {
 	 assert_last_event::<T>(Event::Locked(caller, amount).into());
 	}
 
-	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+	impl_benchmark_test_suite!(Escrow, crate::mock::new_test_ext(), crate::mock::Test);
 }
 
